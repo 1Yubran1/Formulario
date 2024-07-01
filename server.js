726e7e3b58fafd43bc34db634fc40327cb004a7c
1Yubran1 <paulyubranp@gmail.com>
2024-07-01 13:58:20 -0600

    nombre de equipos

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path');
-
 const app = express();
 const port = 3000;
 
@@ -43,217 +41,183 @@ let results = {
     form30: {},
 };
 
-function saveDataToFile(data, filename) {
-    fs.writeFileSync(path.join(__dirname, 'data', filename), JSON.stringify(data, null, 2));
-}
-
 // Endpoint para recibir los resultados del formulario 1 (POST)
 app.post('/submit/form1', (req, res) => {
     results.form1 = req.body;
-    saveDataToFile(data, 'form1.json');
     res.send('Valores de la estación 1.1 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 2 (POST)
 app.post('/submit/form2', (req, res) => {
     results.form2 = req.body;
-    saveDataToFile(data, 'form2.json');
     res.send('Valores de la estación 1.2 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 3 (POST)
 app.post('/submit/form3', (req, res) => {
     results.form3 = req.body;
-    saveDataToFile(data, 'form3.json');
     res.send('Valores de la estación 2.1 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 4 (POST)
 app.post('/submit/form4', (req, res) => {
     results.form4 = req.body;
-    saveDataToFile(data, 'form4.json');
     res.send('Valores de la estación 2.2 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 5 (POST)
 app.post('/submit/form5', (req, res) => {
     results.form5 = req.body;
-    saveDataToFile(data, 'form5.json');
     res.send('Valores de la estación 3.1 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 6 (POST)
 app.post('/submit/form6', (req, res) => {
     results.form6 = req.body;
-    saveDataToFile(data, 'form6.json');
     res.send('Valores de la estación 3.2 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 7 (POST)
 app.post('/submit/form7', (req, res) => {
     results.form7 = req.body;
-    saveDataToFile(data, 'form7.json');
     res.send('Valores de la estación 4.1 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 8 (POST)
 app.post('/submit/form8', (req, res) => {
     results.form8 = req.body;
-    saveDataToFile(data, 'form8.json');
     res.send('Valores de la estación 4.2 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 9 (POST)
 app.post('/submit/form9', (req, res) => {
     results.form9 = req.body;
-    saveDataToFile(data, 'form9.json');
     res.send('Valores de la estación 5.1 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 10 (POST)
 app.post('/submit/form10', (req, res) => {
     results.form10 = req.body;
-    saveDataToFile(data, 'form10.json');
     res.send('Valores de la estación 5.2 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 11 (POST)
 app.post('/submit/form11', (req, res) => {
     results.form11 = req.body;
-    saveDataToFile(data, 'form11.json');
     res.send('Valores de la estación 6.1 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 12 (POST)
 app.post('/submit/form12', (req, res) => {
     results.form12 = req.body;
-    saveDataToFile(data, 'form12.json');
     res.send('Valores de la estación 6.2 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 13 (POST)
 app.post('/submit/form13', (req, res) => {
     results.form13 = req.body;
-    saveDataToFile(data, 'form13.json');
     res.send('Valores de la estación 7.1 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 14 (POST)
 app.post('/submit/form14', (req, res) => {
     results.form14 = req.body;
-    saveDataToFile(data, 'form14.json');
     res.send('Valores de la estación 7.2 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 15 (POST)
 app.post('/submit/form15', (req, res) => {
     results.form15 = req.body;
-    saveDataToFile(data, 'form15.json');
     res.send('Valores de la estación 8.1 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 16 (POST)
 app.post('/submit/form16', (req, res) => {
     results.form16 = req.body;
-    saveDataToFile(data, 'form16.json');
     res.send('Valores de la estación 8.2 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 17 (POST)
 app.post('/submit/form17', (req, res) => {
     results.form17 = req.body;
-    saveDataToFile(data, 'form17.json');
     res.send('Valores de la estación 9.1 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 18 (POST)
 app.post('/submit/form18', (req, res) => {
     results.form18 = req.body;
-    saveDataToFile(data, 'form18.json');
     res.send('Valores de la estación 9.2 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 19 (POST)
 app.post('/submit/form19', (req, res) => {
     results.form19 = req.body;
-    saveDataToFile(data, 'form19.json');
     res.send('Valores de la estación 10.1 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 20 (POST)
 app.post('/submit/form20', (req, res) => {
     results.form20 = req.body;
-    saveDataToFile(data, 'form20.json');
     res.send('Valores de la estación 10.2 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 21 (POST)
 app.post('/submit/form21', (req, res) => {
     results.form21 = req.body;
-    saveDataToFile(data, 'form21.json');
     res.send('Valores de la estación 11.1 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 22 (POST)
 app.post('/submit/form22', (req, res) => {
     results.form22 = req.body;
-    saveDataToFile(data, 'form22.json');
     res.send('Valores de la estación 11.2 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 23 (POST)
 app.post('/submit/form23', (req, res) => {
     results.form23 = req.body;
-    saveDataToFile(data, 'form23.json');
     res.send('Valores de la estación 11.3 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 24 (POST)
 app.post('/submit/form24', (req, res) => {
     results.form24 = req.body;
-    saveDataToFile(data, 'form24.json');
     res.send('Valores de la estación 12.1 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 25 (POST)
 app.post('/submit/form25', (req, res) => {
     results.form25 = req.body;
-    saveDataToFile(data, 'form25.json');
     res.send('Valores de la estación 13.1 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 26 (POST)
 app.post('/submit/form26', (req, res) => {
     results.form26 = req.body;
-    saveDataToFile(data, 'form26.json');
     res.send('Valores de la estación 14.1 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 27 (POST)
 app.post('/submit/form27', (req, res) => {
     results.form27 = req.body;
-    saveDataToFile(data, 'form27.json');
     res.send('Valores de la estación 15.1 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 28 (POST)
 app.post('/submit/form28', (req, res) => {
     results.form28 = req.body;
-    saveDataToFile(data, 'form28.json');
     res.send('Valores de la estación 15.2 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 29 (POST)
 app.post('/submit/form29', (req, res) => {
     results.form29 = req.body;
-    saveDataToFile(data, 'form29.json');
     res.send('Valores de la estación 16.1 recibido');
 });
 
 // Endpoint para recibir los resultados del formulario 30 (POST)
 app.post('/submit/form30', (req, res) => {
     results.form30 = req.body;
-    saveDataToFile(data, 'form30.json');
     res.send('Valores de la estación 17.1 recibido');
 });
 
@@ -262,136 +226,8 @@ app.get('/results', (req, res) => {
     res.json(results);
 });
 
-// Servir archivos estáticos
+// Servir archivos estáticos (opcional, si necesitas servir archivos HTML)
 app.use(express.static('public'));
-
-// Servir el archivo index.html
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-// Servir formularios específicos
-app.get('/form1', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form1.html'));
-});
-
-app.get('/form2', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form2.html'));
-});
-
-// Añade más rutas para los demás formularios según sea necesario
-// Ejemplo:
-app.get('/form3', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form3.html'));
-});
-
-app.get('/form4', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form4.html'));
-});
-
-app.get('/form5', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form5.html'));
-});
-
-app.get('/form6', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form6.html'));
-});
-
-app.get('/form7', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form7.html'));
-});
-
-app.get('/form8', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form8.html'));
-});
-
-app.get('/form9', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form9.html'));
-});
-
-app.get('/form10', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form10.html'));
-});
-
-app.get('/form11', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form11.html'));
-});
-
-app.get('/form12', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form12.html'));
-});
-
-app.get('/form13', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form13.html'));
-});
-
-app.get('/form14', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form14.html'));
-});
-
-app.get('/form15', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form15.html'));
-});
-
-app.get('/form16', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form16.html'));
-});
-
-app.get('/form17', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form17.html'));
-});
-
-app.get('/form18', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form18.html'));
-});
-
-app.get('/form19', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form19.html'));
-});
-
-app.get('/form20', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form20.html'));
-});
-
-app.get('/form21', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form21.html'));
-});
-
-app.get('/form22', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form22.html'));
-});
-
-app.get('/form23', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form23.html'));
-});
-
-app.get('/form24', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form24.html'));
-});
-
-app.get('/form25', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form25.html'));
-});
-
-app.get('/form26', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form26.html'));
-});
-
-app.get('/form27', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form27.html'));
-});
-
-app.get('/form28', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form28.html'));
-});
-
-app.get('/form29', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form29.html'));
-});
-
-app.get('/form30', (req, res) => {
-    res.sendFile(path.join(__dirname, 'form30.html'));
-});
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
