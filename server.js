7e74e2e0a95e52d9d726e4235e507f0201607ec1
1Yubran1 <paulyubranp@gmail.com>
2024-07-02 12:16:51 -0600

    cambio de puerto

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 5500;
 
 app.use(cors()); // Permitir CORS para todas las rutas
 app.use(bodyParser.json());
